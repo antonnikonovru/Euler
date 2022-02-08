4b62baa377713674bde1718de90f04e50ce1b7c1
antonnikonovru <97090710+antonnikonovru@users.noreply.github.com>
2022-02-08 20:50:05 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -8,3 +8,9 @@ def compute():
                 return str(a * b * c)
 if __name__ == "__main__":
     print(compute())
+   
+for a in range(1, 1000):
+    for b in range(1, 1000):
+        c = int((a**2 + b**2)**0.5)
+        if a < b < c and a**2 + b**2 == c**2 and a+ b + c == 1000:
+            print(a, b, c)
